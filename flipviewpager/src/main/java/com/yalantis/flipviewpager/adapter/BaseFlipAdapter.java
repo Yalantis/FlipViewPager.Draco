8e59d5cc3f2eb48c80b4075dc48eff64fef5ea40
Philipp Seifert <philipp.seifert@udo.edu>
2016-01-11 12:50:18 +0100

    - Made clickable views inside the ViewPager clickable. - Added support for programmatically closing if the merge page. - Use RecyclerView. - Updated to API 23.

--- flipviewpager/src/main/java/com/yalantis/flipviewpager/adapter/BaseFlipAdapter.java
+++ flipviewpager/src/main/java/com/yalantis/flipviewpager/adapter/BaseFlipAdapter.java
@@ -1,6 +1,7 @@
 package com.yalantis.flipviewpager.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,98 +11,87 @@ import com.yalantis.flipviewpager.R;
 import com.yalantis.flipviewpager.utils.FlipSettings;
 import com.yalantis.flipviewpager.view.FlipViewPager;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Yalantis
  */
-public abstract class BaseFlipAdapter<T> extends BaseAdapter {
-    private List<T> items;
+public abstract class BaseFlipAdapter extends RecyclerView.Adapter<BaseFlipAdapter.BaseViewHolder> {
+
+    /**
+     * We're saving which FlipViewPager is at which position so we can call flipToPage()
+     * on a close click.
+     */
+    protected Map<Integer, FlipViewPager> flipViewPagerMap = new HashMap<>();
+    private List items;
     private FlipSettings settings;
     private LayoutInflater inflater;
 
-    public BaseFlipAdapter(Context context, List<T> items, FlipSettings settings) {
+    public BaseFlipAdapter(Context context, List items, FlipSettings settings) {
         this.items = items;
         this.settings = settings;
         inflater = LayoutInflater.from(context);
     }
 
     @Override
-    public int getCount() {
-        // Checking if we need an additional row for single item
-        return items.size() % 2 != 0 ? ((items.size() / 2) + 1) : (items.size() / 2);
+    public BaseViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = inflater
+                .inflate(R.layout.flipper, viewGroup, false);
+        BaseViewHolder baseViewHolder = new BaseViewHolder(v);
+        baseViewHolder.mFlipViewPager = (FlipViewPager) v.findViewById(R.id.flip_view);
+        return baseViewHolder;
     }
 
     @Override
-    public T getItem(int position) {
-        return items.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public void onBindViewHolder(BaseViewHolder baseViewHolder, final int position) {
         // Trick to divide list into 2 parts
-        T item1 = getItem(position * 2);
+        Object item1 = items.get(position * 2);
         // Used for cases when we have not an even size in incoming list
-        T item2 = items.size() > (position * 2 + 1) ? getItem(position * 2 + 1) : null;
-
-        final ViewHolder viewHolder;
-        if (convertView == null)
-            convertView = inflater.inflate(R.layout.flipper, null);
-        if (convertView.getTag() != null) {
-            viewHolder = (ViewHolder) convertView.getTag();
-        } else {
-            viewHolder = new ViewHolder();
-            convertView.setTag(viewHolder);
-            viewHolder.mFlipViewPager = (FlipViewPager) convertView.findViewById(R.id.flip_view);
-        }
+        Object item2 = items.size() > (position * 2 + 1) ? items.get(position * 2 + 1) : null;
 
         // Listener to store flipped page
-        viewHolder.mFlipViewPager.setOnChangePageListener(new FlipViewPager.OnChangePageListener() {
+        baseViewHolder.mFlipViewPager.setOnChangePageListener(new FlipViewPager.OnChangePageListener() {
             @Override
             public void onFlipped(int page) {
                 settings.savePageState(position, page);
             }
         });
 
-        if (viewHolder.mFlipViewPager.getAdapter() == null) {
-            viewHolder.mFlipViewPager.setAdapter(new MergeAdapter(item1, item2), settings.getDefaultPage(), position, items.size());
-        } else {
-            // Recycling internal adapter
-            // So, it's double recycling - we have only 4-5 mFlipViewPager objects
-            // and each of them have an adapter
-            MergeAdapter adapter = (MergeAdapter) viewHolder.mFlipViewPager.getAdapter();
-            adapter.updateData(item1, item2);
-            viewHolder.mFlipViewPager.setAdapter(adapter, settings.getPageForPosition(position), position, items.size());
-        }
-        return convertView;
+        flipViewPagerMap.put(position, baseViewHolder.mFlipViewPager);
+        baseViewHolder.mFlipViewPager.setAdapter(new MergeAdapter(item1, item2, position), settings.getDefaultPage(), position, items.size());
     }
 
-    class ViewHolder {
+    @Override
+    public int getItemCount() {
+        // Checking if we need an additional row for single item
+        return items.size() % 2 != 0 ? ((items.size() / 2) + 1) : (items.size() / 2);
+    }
+
+    class BaseViewHolder extends RecyclerView.ViewHolder {
+
         FlipViewPager mFlipViewPager;
+
+        public BaseViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 
-    public abstract View getPage(int position, View convertView, ViewGroup parent, T item1, T item2);
+    public abstract View getPage(int position, View convertView, ViewGroup parent, Object item1, Object item2, CloseListener closeListener);
 
     public abstract int getPagesCount();
 
     // Adapter merges 2 items together
-    private class MergeAdapter extends BaseAdapter {
-        private T item1;
-        private T item2;
+    private class MergeAdapter extends BaseAdapter implements CloseListener {
+        private Object item1;
+        private Object item2;
+        private int position;
 
-        public MergeAdapter(T item1, T item2) {
+        public MergeAdapter(Object item1, Object item2, int position) {
             this.item1 = item1;
             this.item2 = item2;
-        }
-
-        public void updateData(T item1, T item2) {
-            this.item1 = item1;
-            this.item2 = item2;
+            this.position = position;
         }
 
         @Override
@@ -121,7 +111,16 @@ public abstract class BaseFlipAdapter<T> extends BaseAdapter {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return getPage(position, convertView, parent, item1, item2);
+            return getPage(position, convertView, parent, item1, item2, this);
         }
+
+        @Override
+        public void onClickClose() {
+            flipViewPagerMap.get(position).flipToPage(1);
+        }
+    }
+
+    public interface CloseListener {
+        void onClickClose();
     }
 }
